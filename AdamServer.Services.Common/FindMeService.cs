098d7c3bcd0fc925923d1c9514fad1aca3e02c18
Alexey Nesterov <vertigra.developer@gmail.com>
2024-12-19 11:43:25 +1000

    Update FindMe service

--- AdamServer.Services.Common/FindMeService.cs
+++ AdamServer.Services.Common/FindMeService.cs
@@ -11,19 +11,12 @@ namespace AdamServer.Services.Common
     public class FindMeService : BackgroundService, IFindMeService
     {
         private readonly ILogger<FindMeService> mLogger;
-        private readonly Socket mSocket;
-
+        private readonly UdpClient mServer;
+        private IPEndPoint mRemoteEndPoint = new(IPAddress.Any, 11000);
         public FindMeService(IServiceProvider serviceProvider)
         {
             mLogger = serviceProvider.GetRequiredService<ILogger<FindMeService>>();
-            mSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            
-            IPEndPoint ipEndPoint = new(IPAddress.Any, 18000);
-            mSocket.Bind(ipEndPoint);
-
-            IPAddress ip = IPAddress.Parse("224.5.6.7");
-            mSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(ip, IPAddress.Any));
-
+            mServer = new UdpClient(11000);
             mLogger.LogTrace("Start FindMe service!");
         }
 
@@ -34,19 +27,9 @@ namespace AdamServer.Services.Common
             {
                 while (!stoppingToken.IsCancellationRequested)
                 {
-                    mLogger.LogTrace("Start FindMe service reciving message");
-                    byte[] byteArray = new byte[10];
-                    
-                    IPEndPoint ip = new(IPAddress.Any, 0);
-                    EndPoint remoteEndPoint = ip;
-
-                    mSocket.ReceiveFrom(byteArray, byteArray.Length, SocketFlags.None, ref remoteEndPoint);
-                    
-                    EndPoint remoteEndpoint = mSocket.RemoteEndPoint;
-                    string str = Encoding.UTF8.GetString(byteArray, 0, byteArray.Length);
-
-                    mLogger.LogTrace("Remote ep {remoteEndPoint}", remoteEndPoint);
-                    mLogger.LogTrace("RX: {recived}" + str.Trim());
+                    mLogger.LogTrace("FindMe service waiting bradcast...");
+                    byte[] byteArray = mServer.Receive(ref mRemoteEndPoint);
+                    mLogger.LogTrace("Remote ep {remoteEndPoint}", mRemoteEndPoint);
                 }
             }, stoppingToken);
         }
